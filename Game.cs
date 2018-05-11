091578c3f2890e79f2bc543d1153311521769700
pb10001 <atihsa.mihs.ne.ho.da@gmail.com>
2018-05-11 23:03:45 +0900

    プロジェクト ファイルを追加します。

---
+++ Game.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    class Game
+    {
+        public Game()
+        {
+        }
+        Numbers n;
+        int num;
+        int count = 0;
+        int[] digits = new int[] { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+        private bool Init()
+        {
+            Console.Write("桁数を入力してください(1-9): ");
+            var input = Console.ReadLine();
+            return int.TryParse(input, out num);
+        }
+        public void Play()
+        {
+            if (!Init()) return;
+            Shuffle(digits);
+            var arr = digits.Take(num).ToArray();
+            n = new Numbers(arr);
+            bool end = false;
+            while (!end)
+            {
+                int[] array;
+                Console.Write("予想: ");
+                try
+                {
+                    var line = Console.ReadLine();
+                    count++;
+                    if (line.Equals("exit"))
+                    {
+                        return;
+                    }
+                    array = line.Select(x => int.Parse(x.ToString())).ToArray();
+                }
+                catch
+                {
+                    return;
+                }
+                try
+                {
+                    var result = n.Check(array);
+                    Console.WriteLine("{0} eats, {1} bites", result.Item1, result.Item2);
+                    if (result.Item1 == num)
+                    {
+                        end = true;
+                    }
+                }
+                catch (ArgumentException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+            Console.WriteLine("クリア！ {0}回", count);
+        }
+        private void Shuffle(int[] digits)
+        {
+            Random r = new Random();
+            for (int i = 9; i > 0; i--)
+            {
+                var index = r.Next(i);
+                var tmp = digits[index];
+                digits[index] = digits[i];
+                digits[i] = tmp;
+            }
+        }
+    }
+}
---
+++ Numbers.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mastermind
+{
+    class Numbers
+    {
+        public Numbers(int[] nums)
+        {
+            this.nums = nums;
+        }
+        int[] nums;
+        public Tuple<int, int> Check(int[] input)
+        {
+            var eat = 0;
+            var bite = 0;
+            var hash = new HashSet<int>();
+            if (nums.Length != input.Length)
+            {
+                throw new ArgumentException("桁数が不正です");
+            }
+            foreach (var item in input)
+            {
+                if (!hash.Add(item)) throw new ArgumentException("同じ数字は使えません");
+            }
+            int[] result = new int[nums.Length];
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == input[i])
+                {
+                    eat++;
+                }
+                else
+                {
+                    foreach (var item in nums)
+                    {
+                        if (input[i] == item)
+                        {
+                            bite++;
+                        }
+                    }
+                }
+            }
+            return new Tuple<int, int>(eat, bite);
+        }
+    }
+}
